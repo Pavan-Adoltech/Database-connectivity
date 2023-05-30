0a0b70b5b1954b287d7708e7b99d365e9856efaf
Pavunkumar21 <134576531+Pavunkumar21@users.noreply.github.com>
2023-05-30 16:54:39 +0530

    postest.php

--- database2/postest.php
+++ database2/postest.php
@@ -12,13 +12,13 @@ $data=['ownername'=>'sam','street_name'=>'nethajistreet'];
 $p->addPosts($data);
 var_dump($p->getPosts());
 
-
+//update
 $data=['id'=>1,'ownername'=>'Nehal','street_name'=>'mathajistreet'];
 $p->updatePosts($data);
 var_dump($p->getposts());
 
 
-
+//delete
 $p->deletePosts(1);
 var_dump($p->getPosts());
 
